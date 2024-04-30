9ffafa6dce5e4d85c92064a5b39f0e908960e863
Divinitry <SpencerCodeAcc@gmail.com>
2024-04-29 20:39:26 -0400

    working finished project, might still add some more css changes

--- script.js
+++ script.js
@@ -9,28 +9,46 @@ class Pet {
     petFeed() {
         if (this.hunger > 1) {
             this.hunger--
-            // put animation here of it jumping/eating
+            bunnyElement.src = "feedbunnyimg.png"
+            bunnyElement.style.animation = "bunnyEatAnimation 0.3s linear infinite alternate"
+            sleepText.style.display = "block"
+            sleepText.style.color = "black"
+            sleepText.innerText = "NOM NOM NOM"
+            document.querySelectorAll('[class^="cloud"]');
+            document.querySelectorAll('[class^="cloud"]').forEach((cloud) => {
+                cloud.src = "carrot.png"
+            });
+
+            setTimeout(() => {
+                bunnyElement.src = "bunnyforgame.png"
+                bunnyElement.style.animation = "walking 10s infinite"
+                sleepText.style.color = "white"
+                sleepText.innerText = "ZZZZZ"
+                document.querySelectorAll('[class^="cloud"]').forEach((cloud) => {
+                    cloud.src = "pixelclouds.png"
+                });
+            }, 3000)
         }
     }
     petSleeping() {
         if (this.sleepiness > 1) {
             this.sleepiness--;
             document.getElementById("sleepbackground").style.display = "block";
-            document.getElementById("bunny").src = "darkbunnybackground.png"
-            document.getElementById("bunny").style.animation = "sleeping 2s infinite"
-            document.getElementById("sleepingText").style.display = "block";
-            document.getElementById("sleepingText").style.zIndex = "999";
-            const cloudImages = document.querySelectorAll('[class^="cloud"]');
-            cloudImages.forEach((cloud) => {
-                cloud.src = "starimages.png" 
+            bunnyElement.src = "darkbunnybackground.png"
+            bunnyElement.style.animation = "sleeping 2s infinite"
+            sleepText.style.display = "block";
+            sleepText.style.zIndex = "999";
+            document.querySelectorAll('[class^="cloud"]');
+            document.querySelectorAll('[class^="cloud"]').forEach((cloud) => {
+                cloud.src = "starimages.png"
             });
-    
+
             setTimeout(() => {
                 document.getElementById("sleepbackground").style.display = "none";
-                document.getElementById("bunny").src = "bunnyforgame.png"
-                document.getElementById("bunny").style.animation = "walking 10s infinite"
-                cloudImages.forEach((cloud) => {
-                    cloud.src = "pixelclouds.png" 
+                bunnyElement.src = "bunnyforgame.png"
+                bunnyElement.style.animation = "walking 10s infinite"
+                document.querySelectorAll('[class^="cloud"]').forEach((cloud) => {
+                    cloud.src = "pixelclouds.png"
                 });
             }, 3000);
         }
@@ -38,6 +56,24 @@ class Pet {
     petPlay() {
         if (this.boredom > 1) {
             this.boredom--
+            bunnyElement.src = "playbunny.png"
+            bunnyElement.style.animation = "playingAnimation 1s ease-in-out infinite"
+            sleepText.style.display = "block"
+            sleepText.style.color = "black"
+            sleepText.innerText = "WAHOOOO"
+            document.querySelectorAll('[class^="cloud"]').forEach((cloud) => {
+                cloud.src = "discoball.png"
+            })
+
+            setTimeout(() => {
+                bunnyElement.src = "bunnyforgame.png"
+                bunnyElement.style.animation = "walking 10s infinite"
+                sleepText.style.color = "white"
+                sleepText.innerText = "ZZZZZ"
+                document.querySelectorAll('[class^="cloud"]').forEach((cloud) => {
+                    cloud.src = "pixelclouds.png"
+                });
+            }, 3000)
         }
     }
     increaseAge() {
@@ -50,35 +86,44 @@ class Pet {
                 bunnyElement.style.width = (currentWidth + 10) + "px";
                 bunnyElement.style.height = (currentHeight + 10) + "px";
             }
-        }, 20000);}
+            this.oldAgeDead()
+        }, 30000);
+    }
 
-        increaseStats() {
-            setInterval(() => {
-                this.hunger++;
-                hungerNumber.innerText = this.hunger;
-                this.gameOver()
-            }, 12000);
-            setInterval(() => {
-                this.sleepiness++;
-                sleepNumber.innerText = this.sleepiness;
-                this.gameOver()
-            }, 10000)
-            setInterval(() => {
-                this.boredom++
-                boredomNumber.innerText = this.boredom;
-                this.gameOver()
-            }, 9000)
-        }
-        gameOver() {
-            if (this.hunger === 10 || this.sleepiness === 10 || this.boredom === 10) {
-                showTextBox("Game over, you are a bad owner!")
-                setTimeout(() => location.reload(), 4000)
-            }
+    increaseStats() {
+        setInterval(() => {
+            this.hunger++;
+            hungerNumber.innerText = this.hunger;
+            this.gameOver()
+        }, 12000);
+        setInterval(() => {
+            this.sleepiness++;
+            sleepNumber.innerText = this.sleepiness;
+            this.gameOver()
+        }, 10000)
+        setInterval(() => {
+            this.boredom++
+            boredomNumber.innerText = this.boredom;
+            this.gameOver()
+        }, 9000)
+    }
+    gameOver() {
+        if (this.hunger === 10 || this.sleepiness === 10 || this.boredom === 10) {
+            showTextBox("Game over, you are a bad owner!")
+            setTimeout(() => location.reload(), 4000)
+        } 
+    }
+    oldAgeDead() {
+        if (this.age === 15){
+            showTextBox("Your pet has died of old age...")
+            setTimeout(() => location.reload(), 4000)
         }
     }
+}
 // global variable declaration
 let newPet;
 bunnyElement = document.getElementById("bunny")
+sleepText = document.getElementById("sleepingText")
 
 function showTextBox(text) {
     const textBox = document.getElementById("textPopUp")
